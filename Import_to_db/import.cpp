2c85dae20c6110ca0b212e75bfce07c63891c6b8
elik565 <eliseysavich@gmail.com>
2025-03-26 17:01:45 -0400

    Печать полей в csv

---
+++ Import_to_db/import.cpp
@@ -0,0 +1,114 @@
+#include "import.hpp"
+#include <filesystem>
+#include <iostream>
+#include <fstream>
+
+namespace fs = std::filesystem;
+
+
+std::string get_xml_filename(const std::string& xml_path) {
+    size_t pos = xml_path.rfind('/');
+    if (pos != std::string::npos) {
+        return xml_path.substr(pos + 1);  // оставляем только имя файла
+    }
+
+    return xml_path;  // путь и есть имя файла
+}
+
+
+std::vector<std::string> get_fields(const std::string& xml_filename, const std::string& fields_file) {
+    std::ifstream fin(fields_file);
+    if (!fin) {
+        std::cerr << "Ошибка открытия файла с полями таблиц " << fields_file << "!\n";
+        exit(1);
+    }
+
+    std::vector<std::string> fields;
+    std::string line;
+
+    // находим xml файл в файле с полями
+    while (std::getline(fin, line)) {  // читаем файл построчно
+        if (line.find(xml_filename) != std::string::npos) {  // если нашли строку с именем xml файла
+            break;
+        }
+    }
+
+    // читаем поля xml файла построчно
+    while (std::getline(fin, line)) {
+        if (line.find("Поля в файле") != std::string::npos) {  // ищем до следующего файла
+            break;
+        }
+        
+        if (line != "") {
+            fields.push_back(line);
+        }
+    }
+
+    return fields;
+}
+
+
+void xml_to_csv(const std::string& xml_path, const std::string& fields_file, const std::string& csv_directory) {
+    std::ifstream fin(xml_path);
+    if (!fin) {
+        std::cerr << "Ошибка открытия файла " << xml_path << " для конвертации в csv!\n";
+        return;  // не выходим из программы
+    }
+
+    std::string xml_filename = get_xml_filename(xml_path);  // получаем имя xml файла 
+
+    std::vector<std::string> fields = get_fields(xml_filename, fields_file);  // находим поля в этом файле
+
+    if (fields.size() == 0) {  // если не нашли полей xml файла
+        std::cerr << "Поля файла " << xml_path << " не найдены в файле" << fields_file << "!\n";
+        return;  // не выходим из программы
+    }
+    
+    // создаем файл csv
+    std::string csv_path = csv_directory + "/" + xml_filename.substr(0, xml_filename.find('.')) + ".csv";
+    std::ofstream fout(csv_path);
+
+    // печатаем поля в csv файл
+    for (size_t i = 0; i < fields.size(); i++) {
+        fout << fields[i];
+        if (i != fields.size() - 1) {
+            fout << ", ";
+        }
+    }
+    fout << "\n";
+
+    // читаем xml файл построчно
+    std::string line;
+    while (std::getline(fin, line)) {
+        for (size_t i = 0; i < fields.size(); i++) {
+            size_t pos = line.find(fields[i]);  // находим позицию поля
+            if (pos != std::string::npos) {  // если нашли 
+                //fout << line.substr(pos + fields[i].size());
+            }
+        }
+    }
+}
+
+
+void xml_directory_to_csv(const std::string& xml_directory, const std::string& fields_file) {
+    if (!fs::is_directory(xml_directory)) {
+        std::cerr << "Директория " << xml_directory << " не найдена!\n";
+        exit(1);
+    }
+
+    // создаем директорию с csv файлами
+    std::string csv_directory = xml_directory + "_csv";
+    if (!fs::exists(csv_directory)) {
+        if (!fs::create_directory(csv_directory)) {
+            std::cerr << "Директорию с csv файлами " << csv_directory << " не удалось создать!\n";
+            exit(1);
+        }
+    }
+
+    // проходимся по всем xml файлам директории
+    for (const auto& entry : fs::directory_iterator(xml_directory)) {
+        if (entry.is_regular_file() && entry.path().extension() == ".xml") {
+            xml_to_csv(entry.path(), fields_file, csv_directory);
+        }
+    }
+}
---
+++ Import_to_db/import.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+std::string get_xml_filename(const std::string& xml_path);
+
+std::vector<std::string> get_fields(const std::string& xml_filename, const std::string& fields_file);
+
+void xml_to_csv(const std::string& xml_path, const std::string& fields_file, const std::string& csv_directory);
+
+void xml_directory_to_csv(const std::string& xml_directory, const std::string& fields_file);

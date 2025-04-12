dec2b819753450574d6186fd732bf53da0a832f5
elik565 <eliseysavich@gmail.com>
2025-04-12 05:12:19 -0400

    Добавил индекс

--- Q2.sql
+++ Q2.sql
@@ -1,3 +1,8 @@
+CREATE INDEX idx_posts_ans ON "Dump_schema"."Posts" ("AcceptedAnswerId");
+
+ANALYZE "Dump_schema"."Posts";
+ANALYZE "Dump_schema"."Users";
+
 SELECT p1."Id" AS question_id, p2."Id" AS answer_id,
 p2."Score" AS answer_score, u."DisplayName" AS user_name
 FROM "Dump_schema"."Posts" p1
@@ -7,3 +12,5 @@ WHERE p1."PostTypeId" = 1
 AND p1."Tags" LIKE '%postgresql%'
 ORDER BY p2."Score" ASC
 LIMIT 10;
+
+DROP INDEX "Dump_schema".idx_posts_ans;

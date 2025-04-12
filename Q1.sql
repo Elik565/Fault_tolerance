1ce4133f2b00a2233b110a0b68d44e3dd30cec0d
elik565 <eliseysavich@gmail.com>
2025-04-12 05:14:42 -0400

    Убрал explain

--- Q1.sql
+++ Q1.sql
@@ -3,7 +3,6 @@ CREATE INDEX idx_posts_parentid ON "Dump_schema"."Posts"("ParentId");
 ANALYZE "Dump_schema"."Posts";
 ANALYZE "Dump_schema"."Users";
 
-explain analyze
 WITH "PostTagPairs" AS (  -- находим пары тегов для каждого поста
     WITH tags AS (
         SELECT "Id" AS post_id, unnest(string_to_array("Tags", '|')) AS tag
@@ -43,4 +42,4 @@ GROUP BY atc.tag1, atc.tag2, atc.count
 ORDER BY atc.count DESC
 LIMIT 10;
 
-DROP INDEX idx_posts_parentid 
+DROP INDEX "Dump_schema".idx_posts_parentid;
